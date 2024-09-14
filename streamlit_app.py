ef527b4f3ce25701495f038ea6ff991b934ce1a7
SLupita <43573085+SLupita@users.noreply.github.com>
2024-09-14 10:45:26 +0000

    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,9 +40,11 @@ def get_vector_store(text_chunk, api_key):
 
 
 def get_conversational_chain():
-    prompt_template = """Answer the question as detailed as possible from the provided context\n\n
-    context ;\n {context}?\n
-    question: \n {question}?n 
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
 
     Answer:
     """
