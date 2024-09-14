de2062434e28d81e557dfead857b1f329a6ec9ea
SLupita <43573085+SLupita@users.noreply.github.com>
2024-09-14 08:09:33 +0000

    1st draft

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,14 @@
-import streamlit as st
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_openai.embeddings import OpenAIEmbeddings
+import os
 from openai import OpenAI
 
 # Show title and description.
 st.title("💬 Chatbot")
 st.write(
     "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "This uses LangChain + RAG model"
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
