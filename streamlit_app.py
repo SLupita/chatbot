0519cc2dc8271caff78101a0a7086469077e3e53
SLupita <43573085+SLupita@users.noreply.github.com>
2024-09-14 10:38:35 +0000

    bug fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,56 +3,90 @@ from langchain_community.vectorstores import FAISS
 from langchain_openai.embeddings import OpenAIEmbeddings
 import os
 from openai import OpenAI
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_openai.embeddings import OpenAIEmbeddings
+import os
+from langchain_community.document_loaders import PyPDFLoader
+# from PyPDF2 import PdfReader
+from langchain_openai.chat_models import ChatOpenAI
+from langchain.agents.agent_toolkits import create_retriever_tool
+from langchain.agents.agent_toolkits import create_conversational_retrieval_agent
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+import streamlit as st
 
-# Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "This uses LangChain + RAG model"
-)
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdfloader = PyPDFLoader(pdf)
+        pdfpages = pdfloader.load_and_split()
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+        # pdf_reader = PdfReader(pdf)
+        # for page in pdf_reader.pages:
+        #     text = page.extraxt.text()
+    return text
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+def get_chunks(mydocuments):
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(mydocuments)
+    return texts
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+def get_vector_store(text_chunk, api_key):
+    embeddings = OpenAIEmbeddings()
+    vector_store = FAISS.from_documents(text_chunk, embeddings)
+    vector_store.save_local("faiss_index")
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
 
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+def get_conversational_chain():
+    prompt_template = """Answer the question as detailed as possible from the provided context\n\n
+    context ;\n {context}?\n
+    question: \n {question}?n 
 
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
+    Answer:
+    """
+    chat_llm = ChatOpenAI(temperature = 0)
+    prompt = PromptTemplate(template=prompt_template, input_variable = ["context","question"])
+    chain = load_qa_chain(chat_llm, chain_type="stuff", prompt=prompt)
+    return chain
 
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+def user_input(question, api_key):
+    embeddings = OpenAIEmbeddings()
+    new_db = FAISS.load_local("faiss_index", embeddings)
+    docs = new_db.similarity_search(question)
+    chain = get_conversational_chain()
+    response = chain({"input_documents": docs, "question": question}, return_only_outputs=True)
+    st.write("Reply: ", response["output_text"])
+
+
+# if not openai_api_key:
+#     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+# else:
+
+# Streamlit UI
+def main():
+    # Show title and description.
+    st.title("💬 Chatbot")
+    st.write(
+        "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
+        "It uses LangChain with RAG and ChatGPT"
+    )
+
+    # Ask user for their OpenAI API key via `st.text_input`.
+    # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
+    # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+    api_key = st.text_input("OpenAI API Key", type="password")
+
+    with st.sidebar:
+        st.title("Menu")
+        pdf_docs = st.file_uploader("Upload your docs", accept_multiple_files=True, key="pdf_uploader")
+        if st.button("submit & process", ley="process_button") and api_key:
+            with st.spinner("processing.."):
+                text = get_pdf_text(pdf_docs)
+                chunks = get_chunks(text)
+                get_vector_store(chunks, api_key)
+                st.success("Done")
+
+if __name__ == "_main_":
+    main()
